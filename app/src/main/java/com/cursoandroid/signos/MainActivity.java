03e22b96552f9a4af1e6f9efcb37ed7c9ae52286
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-25 19:00:18 -0300

    Ading listview reference at main activity.

--- app/src/main/java/com/cursoandroid/signos/MainActivity.java
+++ app/src/main/java/com/cursoandroid/signos/MainActivity.java
@@ -3,12 +3,19 @@ package com.cursoandroid.signos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listaSignos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaSignos = findViewById(R.id.listview);
+
     }
 }
